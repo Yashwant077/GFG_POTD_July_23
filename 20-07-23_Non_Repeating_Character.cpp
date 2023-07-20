958d2e3aa4f7729bac711fbadc501d2d64e8fe20
Yashwant <84570757+Yashwant077@users.noreply.github.com>
2023-07-20 11:15:29 +0530

    Create 20-07-23_Non_Repeating_Character.cpp

---
+++ 20-07-23_Non_Repeating_Character.cpp
@@ -0,0 +1,55 @@
+/*
+Problem Name: Non Repeating Character
+Description:
+
+Given a string S consisting of lowercase Latin Letters. Return the first non-repeating character in S. If there is no non-repeating character, return '$'.
+
+Example 1:
+
+Input:
+S = hello
+Output: h
+Explanation: In the given string, the
+first character which is non-repeating
+is h, as it appears first and there is
+no other 'h' in the string.
+
+Example 2:
+
+Input:
+S = zxvczbtxyzvy
+Output: c
+Explanation: In the given string, 'c' is
+the character which is non-repeating. 
+Your Task:
+You only need to complete the function nonrepeatingCharacter() that takes string S as a parameter and returns the character. If there is no non-repeating character then return '$' .
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Number of distinct characters).
+Note: N = |S|
+
+Constraints:
+1 <= N <= 10^5
+
+*/
+
+class Solution
+{
+    public:
+    //Function to find the first non-repeating character in a string.
+    char nonrepeatingCharacter(string S)
+    {
+       //Your code here
+        int arr[26] = {0};
+        int n = S.size();
+        
+        for(int i=0; i<n; i++)
+            arr[S[i]-'a']++;
+        
+        for(int i=0; i<n; i++)
+            if(arr[S[i]-'a']==1)
+                return S[i];
+                
+        return '$';
+    }
+};
